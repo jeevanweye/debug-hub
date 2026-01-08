efd98cffadb08f55a679cda47ad0441c40f2acda
Jeevan Gupta <jeevan.gupta@wheelseye.com>
2026-01-08 18:20:53 +0530

    add ui

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -21,16 +21,21 @@ class _DashBoardState extends State<DashBoard> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.amber,
         title: Text("Debug Hub"),
         actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.search, size: 24)),
-          IconButton(onPressed: (){}, icon: Icon(Icons.delete, size: 24)),
-          IconButton(onPressed: (){}, icon: Icon(Icons.import_export, size: 24)),
-          IconButton(onPressed: (){}, icon: Icon(Icons.menu, size: 24)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.search, size: 24)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.delete, size: 24)),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.import_export, size: 24),
+          ),
+          IconButton(onPressed: () {}, icon: Icon(Icons.menu, size: 24)),
         ],
       ),
       body: _pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.amber,
         type: BottomNavigationBarType.fixed,
         currentIndex: _currentIndex,
         onTap: (index) {
--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Debug Hub',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
