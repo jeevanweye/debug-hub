fb70891b61372f1c1b7519cd444ca5110a2c3339
Jeevan Gupta <jeevan.gupta@wheelseye.com>
2026-01-08 18:17:12 +0530

    add ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:debug_hub/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Text("data"),
+      home: DashBoard(),
     );
   }
 }
